f453ef873008475382f1e6c0f8ae236abf649615
Rao MAK <113558414+RaoMukaramAli@users.noreply.github.com>
2025-01-20 17:07:53 +0500

    navbar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,6 @@ import Image from "next/image";
 
 const Header = () => {
   const [isNavOpen, setIsNavOpen] = useState(false);
-  const [isScrolled, setIsScrolled] = useState(false);
 
   const handleToggleNav = () => {
     setIsNavOpen((prev) => !prev);
@@ -15,22 +14,6 @@ const Header = () => {
   const closeNav = () => {
     setIsNavOpen(false);
   };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 50) {
-        setIsScrolled(true);
-      } else {
-        setIsScrolled(false);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -49,22 +32,21 @@ const Header = () => {
       document.removeEventListener("click", handleClickOutside);
     };
   }, [isNavOpen]);
-
   return (
-    <header
-      className={`site-header ${styles.siteHeader} ${
-        isScrolled ? styles.scrolled : ""
-      }`}
-    >
-      <nav className={`navbar navbar-expand-xl ${styles.navbar}`}>
+    <header className={`site-header ${styles.siteHeader} `}>
+      <nav className={`navbar d-flex navbar-expand-xl ${styles.navbar}`}>
         <div className="container">
           <button
-            className={`navbar-toggler d-xl-none ${styles.navbarToggler}`}
+            className={` navbar-toggler d-xl-none ${styles.navbarToggler} `}
             type="button"
             onClick={handleToggleNav}
             aria-expanded={isNavOpen}
             aria-label="Toggle navigation"
-          ></button>
+          >
+            <span className={` ${styles.bar} ${styles.bar1} `}></span>
+            <span className={` ${styles.bar} ${styles.bar2} `}></span>
+            <span className={` ${styles.bar} ${styles.bar3} `}></span>
+          </button>
           <Link href="/">
             <Image
               className="d-none d-md-block"
@@ -82,18 +64,21 @@ const Header = () => {
             ></Image>
           </Link>
 
+          {/* <a href="#" className={` d-xl-none ${styles.mobileLogo} `}></a> */}
+
           <div
             className={`collapse navbar-collapse ${isNavOpen ? "show" : ""} ${
               styles.navbar_collapse
             }`}
             id="navbarSupportedContent"
           >
-            <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item" onClick={closeNav}>
                 <Link className="nav-link" aria-current="page" href="/">
                   Home
                 </Link>
               </li>
+
               <li className="nav-item" onClick={closeNav}>
                 <Link className="nav-link" aria-current="page" href="/aboutus">
                   About Us
@@ -128,14 +113,18 @@ const Header = () => {
                 </ul>
               </li>
               <li className="nav-item" onClick={closeNav}>
-                <Link className="nav-link" aria-current="page" href="/industries">
+                <Link
+                  className="nav-link"
+                  aria-current="page"
+                  href="/industries"
+                >
                   Industries
                 </Link>
               </li>
             </ul>
           </div>
 
-          <div className="d-block">
+          <div className=" d-block ">
             <Link href="/contactus">
               <button className={`btn ${styles.navBtn}`} type="submit">
                 Contact Us
